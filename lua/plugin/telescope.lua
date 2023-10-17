11fe29a9363502a27e4cfc10abd84fc0aac1652a
MICHAEL WELSH <mikewelsh801@gmail.com>
2023-10-16 20:58:57 -0600

    Fixed telescope issue with vertical splits.

--- lua/plugin/telescope.lua
+++ lua/plugin/telescope.lua
@@ -6,3 +6,15 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+require('telescope').setup {
+    defaults = {
+        mappings = {
+            i = {
+                -- change split vertical mapping, so it doesn't conflict
+                -- with pasting (Ctrl-v)
+                ["<C-s>"] = "select_vertical"
+            }
+        }
+    },
+}
