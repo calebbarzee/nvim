4d7ac308666a30f52539fa128e107d7d2f8d6dba
MICHAEL WELSH <mikewelsh801@gmail.com>
2024-01-09 19:13:54 -0700

    Reverted back to only toggling diagnostics with [d keys. Trouble only moves line-by-line, which makes navigating diagnostics slow that way.

--- lua/plugin/trouble.lua
+++ lua/plugin/trouble.lua
@@ -1,13 +1,18 @@
 local trouble = require("trouble")
 
-vim.keymap.set("n", "[d", function()
-  trouble.open();
-  trouble.previous({ skip_groups = true, jump = true });
-end)
-
-vim.keymap.set("n", "]d", function()
-  trouble.open();
-  trouble.next({ skip_groups = true, jump = true });
-end)
+-- vim.keymap.set("n", "[d", function()
+--   trouble.open();
+--   trouble.previous({ skip_groups = true, jump = true });
+-- end)
+--
+-- vim.keymap.set("n", "]d", function()
+--   trouble.open();
+--   trouble.next({ skip_groups = true, jump = true });
+-- end)
 
 vim.keymap.set("n", "<leader>xx", function() trouble.toggle() end)
+vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
+vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
+vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
+vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
+vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
