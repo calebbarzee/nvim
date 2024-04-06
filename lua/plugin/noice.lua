1c1b41d5cad00b3738f6fc199ac89fc8d1576704
MICHAEL WELSH <mikewelsh801@gmail.com>
2024-04-05 21:05:25 -0600

    Added a fix for noice lsp clash.

--- lua/plugin/noice.lua
+++ lua/plugin/noice.lua
@@ -24,6 +24,12 @@ return {
       },
       lsp = {
         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        hover = {
+          enabled = false
+        },
+        signature = {
+          enabled = false,
+        },
         override = {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
           ["vim.lsp.util.stylize_markdown"] = true,
