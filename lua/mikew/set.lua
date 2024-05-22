304b4c3682c307074cd5ff68064f8e33b98c07d1
MICHAEL WELSH <mikewelsh801@gmail.com>
2024-05-22 11:42:51 -0600

    refactor(set_remap): added local variables for easier reading.

--- lua/mikew/set.lua
+++ lua/mikew/set.lua
@@ -1,49 +1,54 @@
+local opt = vim.opt
+
 -- relative line numbers
-vim.opt.nu = true
-vim.opt.relativenumber = true
+opt.nu = true
+opt.relativenumber = true
 -- I'm not sold on this, but I'll try it out.
-vim.opt.cursorline = true
+opt.cursorline = true
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
 
-vim.opt.smartindent = true
+opt.smartindent = true
 
-vim.opt.wrap = false
+opt.wrap = false
 
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+opt.swapfile = false
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
 
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+opt.hlsearch = false
+opt.incsearch = true
+opt.inccommand = "split"
 
-vim.opt.termguicolors = true
+opt.termguicolors = true
 
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+opt.scrolloff = 8
+opt.signcolumn = "yes"
+opt.isfname:append("@-@")
 
-vim.opt.updatetime = 50
+opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
+opt.colorcolumn = "80"
 
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.cmd("set nofoldenable")
 
 -- ***CHANGE TERMINAL THAT OPENS WITH <C-z>***
-vim.opt.shell = "fish"
+opt.shell = "bash"
+opt.shellcmdflag = '-c'
+opt.shellxquote = ''
 
 -- don't split up the status line
-vim.opt.laststatus = 3
+opt.laststatus = 3
 
 -- creates little dots before where you type
-vim.opt.list = true
-vim.opt.listchars = { tab = "⎢ ", trail = '·', nbsp = '␣' }
+opt.list = true
+opt.listchars = { tab = "⎢ ", trail = '·', nbsp = '␣' }
 
 -- sets highlighting when you yank something
 vim.api.nvim_create_autocmd('TextYankPost', {
